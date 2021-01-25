03f1b6d83d815811b5883d80087bce0fa8a2beee
kellynauert <kellynauert@gmail.com>
2021-01-24 21:31:54 -0500

    Added additional error states

--- controllers/logcontroller.js
+++ controllers/logcontroller.js
@@ -21,36 +21,111 @@ router.get('/', validateSession, (req, res) => {
 		.catch((err) => res.status(500).json({ error: err }));
 });
 
-router.get('/:id', (req, res) => {
-	Log.findAll({
-		where: { id: req.params.id },
-	})
-		.then((logs) => res.status(200).json(logs))
-		.catch((err) => res.status(500).json({ error: err }));
+router.get('/:id', validateSession, (req, res) => {
+	const whereUserAndIdMatches = {
+		where: { id: req.params.id, owner_id: req.user.id },
+	};
+
+	Log.findAll(whereUserAndIdMatches)
+		.then(function findSuccess(log) {
+			if (log.length > 0) {
+				Log.findAll(whereUserAndIdMatches).then((log) =>
+					res.status(200).json(log)
+				);
+			} else {
+				res.status(500).json({ error: err });
+			}
+		})
+		.catch((err) =>
+			res.status(404).json({ error: 'You do not own any logs with that ID.' })
+		);
 });
+
 router.put('/:id', validateSession, function (req, res) {
 	const updatelogEntry = {
 		description: req.body.description,
 		definition: req.body.definition,
 		result: req.body.result,
 	};
-
-	const query = { where: { id: req.params.id, owner_id: req.user.id } };
-
-	Log.update(updatelogEntry, query)
-		.then(() => res.status(200).json({ message: 'Entry updated.' }))
-		.catch((err) => res.status(500).json({ error: err }));
-});
-
-router.delete('/:id', validateSession, function (req, res) {
-	const query = {
-		where: {
-			id: req.params.id,
-			owner_id: req.user.id,
-		},
+	const whereIdMatches = { where: { id: req.params.id } };
+	const whereUserMatches = { where: { owner_id: req.user.id } };
+	const whereUserAndIdMatches = {
+		where: { id: req.params.id, owner_id: req.user.id },
 	};
-	Log.destroy(query)
-		.then(() => res.status(200).json({ message: 'Entry removed' }))
+	Log.findOne(whereIdMatches)
+		.then(function findLogSucess(log) {
+			if (log) {
+				Log.findOne(whereUserAndIdMatches).then(function findUserSuccess(
+					match
+				) {
+					if (match) {
+						Log.update(updatelogEntry, whereUserAndIdMatches)
+							.then(() => res.status(200).json({ message: 'Entry updated.' }))
+							.catch((err) => res.status(500).json({ error: err }));
+					} else {
+						Log.findAll(whereUserMatches).then((logs) => {
+							res.status(401).json({
+								error:
+									"That log does not belong to you! Here's a list of logs that belong to you:",
+								logs,
+							});
+						});
+					}
+				});
+			} else {
+				Log.findAll(whereUserMatches).then((logs) => {
+					res.status(404).json({
+						error:
+							"That log does not exist! Here's a list of logs that belong to you:",
+						logs,
+					});
+				});
+			}
+		})
 		.catch((err) => res.status(500).json({ error: err }));
 });
+router.delete('/:id', validateSession, function (req, res) {
+	const whereIdMatches = { where: { id: req.params.id } };
+	const whereUserMatches = { where: { owner_id: req.user.id } };
+	const whereUserAndIdMatches = {
+		where: { id: req.params.id, owner_id: req.user.id },
+	};
+	Log.findOne(whereIdMatches)
+		.then(function findLogSucess(log) {
+			if (log) {
+				Log.findOne(whereUserAndIdMatches).then(function findUserSuccess(
+					match
+				) {
+					if (match) {
+						Log.destroy(whereUserAndIdMatches)
+							.then(() => {
+								res.status(200).json({
+									message: 'Successfully deleted the following log:',
+									match,
+								});
+							})
+							.catch((err) => res.status(500).json({ error: err }));
+					} else {
+						Log.findAll(whereUserMatches).then((logs) => {
+							res.status(401).json({
+								error:
+									"That log does not belong to you! Here's a list of logs that belong to you:",
+								logs,
+							});
+						});
+					}
+				});
+			} else {
+				Log.findAll(whereUserMatches).then((logs) => {
+					res.status(404).json({
+						error:
+							"That log does not exist! Here's a list of logs that belong to you:",
+						logs,
+					});
+				});
+			}
+		})
+		.catch((err) => res.status(500).json({ error: err }));
+});
+
 module.exports = router;
